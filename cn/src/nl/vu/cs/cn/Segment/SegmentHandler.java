60ea03dc8617bf62fd0ae2c5150a2083d44ddf6d
Haritha28 <haritha.h20@gmail.com>
2017-01-02 18:23:35 +0530

    Added the class for handlingSegments at Listener

---
+++ cn/src/nl/vu/cs/cn/Segment/SegmentHandler.java
@@ -0,0 +1,8 @@
+package cn.src.nl.vu.cs.cn.Segment;
+
+/**
+ * Created by haritha on 2/1/17.
+ */
+
+public class SegmentHandler {
+}
---
+++ cn/src/nl/vu/cs/cn/Segment/SegmentsArriveListener.java
@@ -0,0 +1,10 @@
+package cn.src.nl.vu.cs.cn.Segment;
+
+/**
+ * Created by haritha on 2/1/17.
+ */
+
+public class SegmentsArriveListener {
+
+    public void  OnSegmentArrive (Segment segment);
+}
