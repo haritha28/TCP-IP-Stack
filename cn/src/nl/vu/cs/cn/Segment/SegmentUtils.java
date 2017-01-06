56c8fb83911ecb11c29c991bd0dc4a01190326ba
Haritha28 <haritha.h20@gmail.com>
2017-01-06 20:33:56 +0530

    getSYNACK packet is added

---
+++ cn/src/nl/vu/cs/cn/Segment/SegmentUtils.java
@@ -0,0 +1,20 @@
+package cn.src.nl.vu.cs.cn.Segment;
+
+import cn.src.nl.vu.cs.cn.TranmissionControlBlock;
+
+/**
+ * Created by haritha on 6/1/17.
+ */
+
+public class SegmentUtils {
+
+
+    public static Segment getSYNACKPacket (TranmissionControlBlock tcb, long seqno, long ack) {
+
+        //get the packet and return the segment is true
+
+
+    }
+
+
+}
