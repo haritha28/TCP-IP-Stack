24291eaad3539de2d8e2843f58bb9468a257fe6f
Haritha28 <haritha.h20@gmail.com>
2017-01-07 15:46:01 +0530

    Added the structure of class SegmentUtils

--- cn/src/nl/vu/cs/cn/Segment/SegmentUtils.java
+++ cn/src/nl/vu/cs/cn/Segment/SegmentUtils.java
@@ -9,6 +9,12 @@ import cn.src.nl.vu.cs.cn.TranmissionControlBlock;
 public class SegmentUtils {
 
 
+    public static Segment getSYNPacket (TranmissionControlBlock tcb, long seqno, long ack) {
+
+
+    }
+
+
     public static Segment getSYNACKPacket (TranmissionControlBlock tcb, long seqno, long ack) {
 
         //get the packet and return the segment is true
@@ -16,5 +22,15 @@ public class SegmentUtils {
 
     }
 
+    public static Segment getPacket (TranmissionControlBlock tcb, long seqno, long ack) {
+
+
+    }
+
+    public static  getFINPacket (TranmissionControlBlock tcb, long seqno, long ack) {
+
+
+    }
+    
 
 }
