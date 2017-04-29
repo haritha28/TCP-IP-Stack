ab36aae83a0ea40420d44b89377b0effb2843f05
Haritha28 <haritha.h20@gmail.com>
2017-04-29 17:48:02 +0530

    Done

--- cn/src/nl/vu/cs/cn/Segment/SegmentUtils.java
+++ cn/src/nl/vu/cs/cn/Segment/SegmentUtils.java
@@ -31,15 +31,45 @@ public class SegmentUtils {
     }
 
     public static Segment getPacket (TranmissionControlBlock tcb, long seqno, long ack) {
-
-
-
+            Segment segment = new Segment(
+                    tcb.getLocalAddr(),
+                    tcb.getLocalPort(),
+                    seqno,ack);
+        return segment;
     }
 
-    public getFINPacket(TranmissionControlBlock tcb, long seqno, long ack) {
+    public static  Segment getFINPacket(TranmissionControlBlock tcb, long seqno, long ack) {
+        Segment segment = new Segment(
+                tcb.getLocalAddr(),
+                tcb.getLocalPort(),
+                ack);
 
-
+        segment.setIsFin(true);
+        return segment;
     }
 
+    public static boolean inWindow(long left, long seq, long right) {
+
+        boolean inWindow = (left <= right)
+                ? (left <= seq && seq < right)
+                : !(right <= seq && seq < left);
+
+        return inWindow;
+    }
+
+    public static boolean overlap(long left1, long right1, long left2, long right2) {
+        boolean overlap = inWindow(left1, left2, right1) || inWindow(left2, left1, right2))
+                return overlap;
+    }
+
+    public static boolean isAcked(Segment segment, long ack) {
+        return SegmentUtils.inWindow(segment.getSeqno(), segment.getLastSeqno(), ack);
+    }
+
+    public static boolean isAcked(long seq, long ack, int segmentLen) {
+        long lastseq = (segmentLen > 0) ? (seq + segmentLen - 1) % Integer.MAX_VALUE :seq;
+        return SegmentUtils.inWindow(seq, lastseq, ack);
+
+    }
 
 }
